b3f4611c62d7111bb8e7059e83d0b7ddb7f839b0
Philip Hansen <skunkelmusen@gmail.com>
2021-09-16 03:59:43 +0200

    First test with HomeLab

---
+++ server.js
@@ -0,0 +1,24 @@
+const { Console } = require("console");
+const { exec } = require("child_process");
+const express = require("express");
+const app = express();
+
+app.get('/', (req, res) => {
+exec("ls -la", (error, stdout, stderr) => {
+    if (error) {
+        //console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        //console.log(`stderr: ${stderr}`);
+        return;
+    }
+    res.send(`${stdout}`)
+    //console.log(`stdout: ${stdout}`);
+});
+})
+app.listen(3000, function() {
+  console.log("Running. Port 3000.");
+});
+
+console.log('Node server running. Port 3000');
